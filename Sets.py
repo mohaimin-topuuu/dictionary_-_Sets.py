249b91767e6eca69fd925f65352ace2557d5f029
Mohaimin Topu <abmtopu73@gmail.com>
2025-01-19 14:03:00 +0600

    SETs practice done

---
+++ Sets.py
@@ -0,0 +1,10 @@
+# Set is the collection of the underordered items 
+# Each element in the set must be unique and immutable.
+
+nums = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+nums.add(10)
+nums.remove(4)
+print(nums.pop())
+nums.remove(5)
+nums.clear()
